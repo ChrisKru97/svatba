171cf35a0f9cae3e0650945f07b27f197d9c2935
christian.krutsche <christian.krutsche@regiojet.cz>
2021-12-25 17:51:11 +0100

    feat: ssr with firebase

---
+++ firebase/clientApp.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+initializeApp( {
+    apiKey: "AIzaSyDLpb9_cPW9sen4juWVq8zp7rU-De5QeyY",
+    authDomain: "svatba-ac848.firebaseapp.com",
+    projectId: "svatba-ac848",
+    storageBucket: "svatba-ac848.appspot.com",
+    messagingSenderId: "925148974173",
+    appId: "1:925148974173:web:33a0cb1832ec1c3fe051b4"
+  });
+
+ const firestore = getFirestore();
+export {firestore}
