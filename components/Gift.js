9c5849a06205d523c0da37901faa8c414fbd0da2
christian.krutsche <christian.krutsche@regiojet.cz>
2021-12-30 21:14:49 +0100

    added compatibility for phones

--- components/Gift.js
+++ components/Gift.js
@@ -1,9 +1,10 @@
+import Image from "next/image"
 import { useEffect, useState } from "react"
 import { onGiftUpdate, updateGift } from "../firebase/firestore-functions"
 
 const Gift = (props) => {
     const [gift, setGift] = useState(props)
-    const {max = 1, reserved = 0, title, url, image, id} = gift
+    const {max, reserved = 0, title, url, image, id} = gift
 
     useEffect(() => {
         const unsub = onGiftUpdate(id, ({reserved: nextReserved}) => {
@@ -14,25 +15,19 @@ const Gift = (props) => {
         }
     }, [gift, id, reserved])
 
-    return <div className="flex items-center space-x-5 bg-stone-100 p-5 rounded-xl cursor-pointer" onClick={() =>{
+    return <div className="flex flex-col space-y-5 lg:space-y-0 lg:flex-row items-center lg:space-x-5 bg-stone-100 p-5 rounded-xl cursor-pointer" onClick={() =>{
         window.location.href = url
     }}>
         <h2>{title}</h2>
+        {max && 
         <span>{reserved} / {max}</span>
+}
         {image && <img src={image} width={100} alt={`image ${title}`}/>}
-        <img onClick={(e) => {
-            e.stopPropagation()
-            if(reserved <= 0) return
-            updateGift(id, -1)
-        }} src="https://img.icons8.com/external-dreamstale-lineal-dreamstale/32/000000/external-minus-ui-dreamstale-lineal-dreamstale-2.png"
-            width={30} height={30} alt="remove" />
-        <img onClick={(e) => {
+            { max && reserved < max && <div><Image src="/images/plus.svg" height={28} width={28} alt="add" onClick={(e) => {
             e.stopPropagation()
             if(reserved >= max) return
             updateGift(id, 1)
-        }} src="https://img.icons8.com/material-outlined/48/000000/plus--v1.png"
-                  width={30} height={30} alt="add" />
-</div>
+        }}/></div>}</div>
 }
 
 export default Gift
