e01b0d54d659d81e107d9a615f2be43d5e8206c4
christian.krutsche <christian.krutsche@regiojet.cz>
2022-01-06 18:47:41 +0100

    added inview animations

--- components/Info.js
+++ components/Info.js
@@ -1,14 +1,39 @@
-const Info = ({ title, description, iconUrl }) => (
-  <div
-    className="lg:max-w-sm m-5 items-center flex flex-col space-y-3 bg-stone-200 shadow-xl p-5 rounded-xl"
-    key={title}>
-    <img src={iconUrl} width={50} height={50} alt={`icon ${title}`} />
-    <h3 className="text-lg underline">{title}</h3>
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+const Info = ({ title, description, iconUrl }) => {
+  const [bounced, setBounced] = useState(false);
+  const { ref, inView } = useInView({
+    delay: 500,
+    triggerOnce: true,
+    fallbackInView: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setTimeout(() => setBounced(true), 800);
+    } else {
+      setBounced(false);
+    }
+  }, [inView]);
+
+  return (
     <div
-      className="text-sm"
-      dangerouslySetInnerHTML={{ __html: description }}
-    />
-  </div>
-);
+      ref={ref}
+      style={{
+        transition: 'transform 800ms',
+        transform: inView && !bounced ? 'scale(1.1)' : undefined,
+      }}
+      className="lg:max-w-sm m-5 items-center flex flex-col space-y-3 bg-stone-200 shadow-xl p-5 rounded-xl"
+      key={title}>
+      <img src={iconUrl} width={50} height={50} alt={`icon ${title}`} />
+      <h3 className="text-lg underline">{title}</h3>
+      <div
+        className="text-sm"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+    </div>
+  );
+};
 
 export default Info;
