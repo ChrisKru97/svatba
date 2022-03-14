2b4e000d7f64832708e257a6637c5ce1b56cc64a
christian.krutsche <christian.krutsche@regiojet.cz>
2022-03-14 10:55:33 +0100

    fix: last updates

--- components/Gift.js
+++ components/Gift.js
@@ -1,33 +1,30 @@
-import { useEffect, useState } from 'react';
-import { onGiftUpdate } from '../firebase/firestore-functions';
-
 const Gift = (props) => {
-  const [gift, setGift] = useState(props);
-  const { max, reserved = 0, title, url, image, id } = gift;
+  // const [gift, setGift] = useState(props);
+  const { max, reserved = 0, title, url, image, imageUrl, id: _ } = props; //gift;
 
-  useEffect(() => {
-    const unsub = onGiftUpdate(id, ({ reserved: nextReserved }) => {
-      if (nextReserved !== reserved)
-        setGift({ ...gift, reserved: nextReserved });
-    });
-    return () => {
-      unsub();
-    };
-  }, [gift, id, reserved]);
+  // useEffect(() => {
+  //   const unsub = onGiftUpdate(id, ({ reserved: nextReserved }) => {
+  //     if (nextReserved !== reserved)
+  //       setGift({ ...gift, reserved: nextReserved });
+  //   });
+  //   return () => {
+  //     unsub();
+  //   };
+  // }, [gift, id, reserved]);
 
   return (
     <div
       className="flex flex-col space-y-5 lg:space-y-0 lg:flex-row items-center lg:space-x-5 bg-stone-100 p-5 rounded-xl cursor-pointer"
-      onClick={() => {
-        window.location.href = url;
-      }}>
+      onClick={() => window.open(url, '_blank')}>
       <h2 className="underline text-blue-600">{title}</h2>
       {max && (
         <span>
           Rezervováno:&nbsp;{reserved} / {max}
         </span>
       )}
-      {image && <img src={image} width={100} alt={`image ${title}`} />}
+      {(image || imageUrl) && (
+        <img src={image || imageUrl} width={100} alt={`image ${title}`} />
+      )}
       {/* {max && reserved < max && (
         <div>
           <img
